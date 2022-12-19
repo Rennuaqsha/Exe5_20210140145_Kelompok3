1a45cc271562eee165363de2680c0f20d9ea560a
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-12-19 17:37:50 +0800

    add class node

--- Exe5_20210140145_Kelompok3/Program.cs
+++ Exe5_20210140145_Kelompok3/Program.cs
@@ -2,6 +2,13 @@
 
 namespace Exercise_5
 {
+    class node
+    {
+        public int rollNumber;
+        public string name;
+        public node next;
+    }
+
     class program
     {
         
