7a21c8f6f7035a643527201e91c6f2cc6287e68a
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-12-19 17:48:19 +0800

    create class list

--- Exe5_20210140145_Kelompok3/Program.cs
+++ Exe5_20210140145_Kelompok3/Program.cs
@@ -4,9 +4,15 @@ namespace Exercise_5
 {
     class node
     {
+        int ren, san, max = 5;
         public int rollNumber;
         public string name;
         public node next;
+    }
+
+    class list
+    {
+
     }
 
     class program
