e1b62d05b38f599e4f3fc7b9f128e43339330771
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-12-19 17:57:21 +0800

    create bool dellnode

--- Exe5_20210140145_Kelompok3/Program.cs
+++ Exe5_20210140145_Kelompok3/Program.cs
@@ -19,10 +19,62 @@ namespace Exercise_5
             ren = null;
         }
 
-    } 
+        public void addNote()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\n Enter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Enter the roll number of the student: ");
+            nm = Console.ReadLine();
+            node newnode = new node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            //if the node to be interested is the first node
+            if (ren != null || (rollNo == ren.rollNumber))
+            {
+                if ((ren != null) && (rollNo == ren.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = ren;
+                ren = newnode;
+                return;
+            }
+
+            node previous, san;
+            previous = san;
+            san = san;
+            while ((san != null) && (rollNo >= san.rollNumber))
+            {
+                if (rollNo == san.rollNumber)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                previous.next = san;
+                previous.next = newnode;
+            }
+            newnode.next = san;
+            previous.next = newnode;
+        }
+
+        public bool delnode(int rollNo)
+        {
+            node ren, san;
+            ren = san = null;
+            if (search(rollNo, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == START)
+                START = START.next;
+            return true;
+        }
+    }
 
     class program
     {
-        
+
     }
 }
